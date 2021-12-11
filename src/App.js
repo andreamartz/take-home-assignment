85651693f414fa085722445875f6f2eefa6131b1
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-12-11 11:04:37 -0600

    Make indentation consistent

--- src/App.js
+++ src/App.js
@@ -24,43 +24,43 @@ function App() {
       <header>
         <h1>Career Lab text-case converter</h1>
       </header>
-        <form onSubmit={handleSubmit}>
-          <div className="form-control form-control__text">
-            <label htmlFor="text">Text to be converted:</label>
-            <textarea
-              id="text"
-              onChange={handleTextareaChange}
-              value={textInput}
-            />
-          </div>
-          <div className="form-control form-control__radio">
-            <input
-              type="radio"
-              name="conversion"
-              id="conversion-0"
-              value="lowercase"
-              checked={conversionMode === "lowercase"}
-              onChange={handleRadioChange}
-            />
-            <label htmlFor="conversion-0">Convert text to lowercase</label>
-          </div>
-          <div className="form-control form-control__radio">
-            <input
-              type="radio"
-              name="conversion"
-              id="conversion-1"
-              value="uppercase"
-              checked={conversionMode === "uppercase"}
-              onChange={handleRadioChange}
-            />
-            <label htmlFor="conversion-1">Convert text to uppercase</label>
-          </div>
-          <input type="submit" value="Submit" />
-          <div className="result-wrapper form-control form-control__text">
-            <label htmlFor="result">Converted text:</label>
-            <output id="result">{textOutput}</output>
-          </div>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <div className="form-control form-control__text">
+          <label htmlFor="text">Text to be converted:</label>
+          <textarea
+            id="text"
+            onChange={handleTextareaChange}
+            value={textInput}
+          />
+        </div>
+        <div className="form-control form-control__radio">
+          <input
+            type="radio"
+            name="conversion"
+            id="conversion-0"
+            value="lowercase"
+            checked={conversionMode === "lowercase"}
+            onChange={handleRadioChange}
+          />
+          <label htmlFor="conversion-0">Convert text to lowercase</label>
+        </div>
+        <div className="form-control form-control__radio">
+          <input
+            type="radio"
+            name="conversion"
+            id="conversion-1"
+            value="uppercase"
+            checked={conversionMode === "uppercase"}
+            onChange={handleRadioChange}
+          />
+          <label htmlFor="conversion-1">Convert text to uppercase</label>
+        </div>
+        <input type="submit" value="Submit" />
+        <div className="result-wrapper form-control form-control__text">
+          <label htmlFor="result">Converted text:</label>
+          <output id="result">{textOutput}</output>
+        </div>
+      </form>
     </div>
   );
 }
